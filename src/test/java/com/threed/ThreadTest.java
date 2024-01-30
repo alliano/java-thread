9695486f755e69e9c6c27aaa18dc6c8d21aced37
alliano <allianoanonymous@gmail.com>
2024-01-31 07:50:49 +0900

    add delay job and repeted job

--- src/test/java/com/threed/ThreadTest.java
+++ src/test/java/com/threed/ThreadTest.java
@@ -1,5 +1,8 @@
 package com.threed;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.junit.jupiter.api.Test;
 
 import lombok.SneakyThrows;
@@ -65,5 +68,78 @@ public class ThreadTest {
 
         thread1.join();
         thread2.join();
+    }
+
+    @Test @SneakyThrows
+    public void notifyAllTest() {
+        Thread thread1 = new Thread(() -> extracted(), "thread-1");
+        Thread thread2 = new Thread(() -> extracted(), "thread-2");
+        Thread thread3 = new Thread(() -> extracted(), "thread-3");
+
+        Thread thread4 = new Thread(() -> {
+            synchronized(lock) {
+                name = "Abdillah Alli";
+                lock.notifyAll();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+        thread4.join();
+    }
+
+    @SneakyThrows
+    private void extracted() {
+        synchronized(lock) {
+            lock.wait();
+            System.out.println("Name : "+name);
+        }
+    }
+
+    @Test @SneakyThrows
+    public void delayJob(){
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Job one executed......");
+            }
+        };
+
+        Timer timer = new Timer();
+        /**
+         * timer task akan dieksekusi
+         * setelah 5 detik
+         */
+        timer.schedule(timerTask, 5000L);
+
+        Thread.sleep(6000L);
+    }
+
+    @Test @SneakyThrows
+    public void repetedJob(){
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Job repeted executed.....");
+            }
+        };
+
+        Timer timer = new Timer();
+        /**
+         * timerTask akan dieksekusi 1 detik sekali
+         * dan diulangi eksekusi tersebut setelah 2 detik
+         * 
+         * simpelnya eksekusi setelh 1 detik dan istirahat selama 2 detik
+         * dan proses tersebut berulang-ulang kali
+         */
+        timer.schedule(timerTask, 1000L, 2000L);
+
+        Thread.sleep(5000L);
     }
 }
